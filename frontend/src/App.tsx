29bc34b0619c08c2f13dc6c0b3026bc85fd09c46
krishna-nishant <nishantkrishna2005@gmail.com>
2025-02-01 23:08:56 +0530

    add authentication handling and private route for home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,24 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
+import { useState } from "react";
+import RefreshHandler from "./RefreshHandler";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }: any) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
-    <div>
+    <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
